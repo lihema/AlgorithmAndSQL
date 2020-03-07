8bb0562c8cfe71b5e1788d16575bbb197869b129
lihema <13001928196@163.com>
2020-03-07 10:05:33 +0800

    test

--- AlgorithmStudy/src/main/java/StreamStudyTest/StreamTest.java
+++ AlgorithmStudy/src/main/java/StreamStudyTest/StreamTest.java
@@ -3,7 +3,11 @@
  * @date 2020/3/5 4:24 下午
  * @content:
  */
-package java.StreamStudyTest;
+package StreamStudyTest;
 
 public class StreamTest {
+    public static void main(String[] args) {
+
+    }
+
 }
