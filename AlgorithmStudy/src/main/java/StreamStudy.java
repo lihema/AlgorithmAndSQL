383982f4e1633a65b2b1d92d6f22d56be5ef4a6b
李冀舒 <lijishu3@jd.com>
2020-03-22 22:45:55 +0800

    test

--- AlgorithmStudy/src/main/java/StreamStudy.java
+++ AlgorithmStudy/src/main/java/StreamStudy.java
@@ -7,12 +7,13 @@
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class StreamStudy {
 
 
 
-        public List<Integer> initDate(){
+        public List<Integer> initData(){
             List<Integer> skuList = new ArrayList<Integer>();
             for (int i = 0; i <100 ; i++) {
                 skuList.add(i);
@@ -20,15 +21,36 @@ public class StreamStudy {
             return  skuList;
         }
 
+    public List<String> initStringData(){
+        List<String> skuList = new ArrayList<String>();
+        for (int i = 0; i <100 ; i++) {
+            if (i>10) {
+                skuList.add("test" + i);
+            }else {
+                skuList.add("Qwer"+i);
+            }
+        }
+        return  skuList;
+    }
+
         public Long countSku(){
-            Long count=initDate().stream().count();
+            Long count=initData().stream().count();
             System.out.println(count);
             return count;
         }
 
         public static void main(String[] args) {
             StreamStudy streamTest=new StreamStudy();
-            streamTest.countSku();
+            //streamTest.countSku();
+           List<String> initStringData = streamTest.initStringData();
+            Optional<String> largest = initStringData.stream().max(String::compareToIgnoreCase);
+           // System.out.println(largest);
+
+           // Optional<String> startWithQ = initStringData.stream().filter(s -> s.startsWith("Q")).findFirst();
+
+            Optional<String> startWithQ = initStringData.stream().parallel().filter(s -> s.startsWith("Q")).findAny();
+
+            System.out.println(startWithQ);
         }
     }
 
--- AlgorithmStudy/src/test/java/doc/Java8NewFeature.md
+++ AlgorithmStudy/src/test/java/doc/Java8NewFeature.md
@@ -1,4 +1,4 @@
-#Java SE 8 流库
+_#Java SE 8 流库
 ##产生原因
 1.针对集合进行操作，告诉java需要怎么做
 
@@ -17,3 +17,37 @@
 2.指定将初始流转化为其他流的中间操作（惰性操作）
 
 3.应用终止操作，产生结果
+
+##约简
+
+约简指将流值转化为程序中使用的非流值
+
+Optional<T> max(Comparator<? super T> comparator) 
+
+Optional<T> max(Comparator<? super T> comparator) 
+
+根据比较器产生一个Optional对象的。此对象中装着这个流的最大元素和最小元素（都是对流对象的操作），流为空返回一个空的Optional对象
+
+Optional<T> findFirst()
+
+Optional<T> findAny()
+
+分别产生这个流的第一个和任意一个元素，如果这个流为空，会产生一个空的Optional对象
+
+boolean anyMatch(Predicate<? super T> predicate)
+
+boolean allMatch(Predicate<? super T> predicate)
+
+boolean noneMatch(Predicate<? super T> predicate)
+
+分别在这个流中任意元素、所有元素和没有任何元素匹配给定断言时返回true
+
+##Optional类型
+
+
+Optional<T>对象是一种包装器对象,核心作用是在值不存在的时候，会产生一个可替代物。
+
+T orElse(T other)  产生这个Optional的值，或者在该Optional为空时，产生other
+
+T orElseGet(Supplier<? extends T>other)
+
