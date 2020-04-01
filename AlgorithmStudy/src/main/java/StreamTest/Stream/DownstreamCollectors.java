3230c454e95c5b8bf1c11ee66be59b458cc497b3
李冀舒 <lijishu3@jd.com>
2020-04-01 18:24:59 +0800

    增加新的内容

--- AlgorithmStudy/src/main/java/StreamTest/Stream/DownstreamCollectors.java
+++ AlgorithmStudy/src/main/java/StreamTest/Stream/DownstreamCollectors.java
@@ -1,5 +1,15 @@
 package StreamTest.Stream;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.*;
+
 /**
  * @author: LiJiShu
  * @date: 2020/3/31 19:57
@@ -7,13 +17,16 @@ package StreamTest.Stream;
  */
 public class DownstreamCollectors {
 
-    public static class City{
+    public static class City {
 
         private String name;
 
         private String state;
 
         private int population;
+
+        public City(String s, String s1, int parseInt) {
+        }
 
         public String getName() {
             return name;
@@ -40,6 +53,46 @@ public class DownstreamCollectors {
         }
     }
 
+    public static Stream<City> readCities(String filename) throws IOException {
 
+        return Files.lines(Paths.get(filename)).map(l -> l.split(",")).map(a -> new City(a[0], a[1], Integer.parseInt(a[2])));
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        Stream<Locale> locales = Stream.of(Locale.getAvailableLocales());
+        Map<String, Set<Locale>> countryToLocaleSet = locales.collect(groupingBy(Locale::getCountry, toSet()));
+        System.out.println("countryToLocaleSet:" + countryToLocaleSet);
+
+
+        locales = Stream.of(Locale.getAvailableLocales());
+        Map<String, Long> countryToLocaleCount = locales.collect(groupingBy(Locale::getCountry, counting()));
+        System.out.println("countryToLocaleCount:" + countryToLocaleCount);
+
+        Stream<City> cities = readCities("StreamTest/cities.txt");
+        Map<String, Integer> stateToCityPopulation = cities.collect(groupingBy(City::getState
+                , summingInt(City::getPopulation)));
+        System.out.println("stateToCityPopulation:" + stateToCityPopulation);
+
+        cities = readCities("StreamTest/cities.txt");
+        Map<String, Optional<String>> stateToLongestCityName = cities.collect(groupingBy(
+                City::getState
+                , mapping(City::getName
+                        , maxBy(Comparator.comparing(String::length)))));
+        System.out.println("stateToLongestCityName:" + stateToLongestCityName);
+
+        locales = Stream.of(Locale.getAvailableLocales());
+        Map<String, Set<String>> countoryToLanguages = locales.collect(groupingBy(Locale::getDisplayCountry
+                , mapping(Locale::getDisplayLanguage
+                        ,toSet())));
+        System.out.println("countoryToLanguages:" + countoryToLanguages);
+
+        cities = readCities("StreamTest/cities.txt");
+        Map<String,IntSummaryStatistics> stateToCityPopulationSummary = cities
+                                                                     .collect(groupingBy(City::getState),ww)
+
+
+
+    }
 
 }
